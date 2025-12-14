3633947e47ac42999d3afc097c5d8f615dc94255
Soth911 <puthpissoth@gmail.com>
2025-12-14 15:23:52 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -2,32 +2,32 @@
 
 int main(){
     graph p;
-    p.addRoute("A","B",2,100);
-    p.addRoute("A","B",2,100);
-    p.addRoute("A","C",3,150);
-    p.addRoute("A","E",3,150);
-    p.addRoute("E","D",4,200);
-    p.addRoute("C","D",1,120);
-    p.addRoute("B","D",1,120);
+    p.addRoute("PHN","SIN",2,100);
+    p.addRoute("PHN","SIN",2,100);
+    p.addRoute("PHN","ICN",3,150);
+    p.addRoute("PHN","TPE",3,150);
+    p.addRoute("TPE","USA",4,200);
+    p.addRoute("ICN","USA",1,120);
+    p.addRoute("SIN","USA",1,120);
     p.printRoute();
     cout<<endl;
-    p.reachability("A");
+    p.reachability("PHN");
     cout<<endl;
-    p.reachability("B");
+    p.reachability("SIN");
     cout<<endl;
-    p.pathPrint("A","D");
-    cout<<endl;
-    cout<<endl;
-    p.pathLowPrice("A","D");
+    p.pathPrint("PHN","USA");
     cout<<endl;
     cout<<endl;
-    p.pathHighPrice("A","D");
+    p.pathLowPrice("PHN","USA");
     cout<<endl;
     cout<<endl;
-    p.pathLowtime("A","D");
+    p.pathHighPrice("PHN","USA");
     cout<<endl;
     cout<<endl;
-    p.pathHightime("A","D");
+    p.pathLowtime("PHN","USA");
+    cout<<endl;
+    cout<<endl;
+    p.pathHightime("PHN","USA");
     cout<<endl;
     cout<<endl;
     
